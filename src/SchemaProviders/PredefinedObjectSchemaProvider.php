4e52f1d7b2c9db0f3511b3971ceafc1f4cbb4acc
pjordaan <pjordaan@users.noreply.github.com>
2026-09-06 22:30:44 +0000

    Update file(s) "packages/schema-generator/." from "apie-lib/apie-lib-monorepo"

--- src/SchemaProviders/PredefinedObjectSchemaProvider.php
+++ src/SchemaProviders/PredefinedObjectSchemaProvider.php
@@ -6,6 +6,8 @@ use Apie\SchemaGenerator\Interfaces\SchemaProvider;
 use BcMath\Number;
 use cebe\openapi\spec\Components;
 use cebe\openapi\spec\Schema;
+use Closure;
+use DateInterval;
 use DOMAttr;
 use DOMElement;
 use FFI\CData;
@@ -35,6 +37,8 @@ class PredefinedObjectSchemaProvider implements SchemaProvider
                 SimpleXMLElement::class,
                 DOMAttr::class,
                 DOMElement::class,
+                DateInterval::class,
+                Closure::class,
             ]
         );
     }
